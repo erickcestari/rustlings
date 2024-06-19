a0d2905c9c16034fa95492aa6e01a3485590063f
Erick 光 <erickcestari03@gmail.com>
2024-06-19 15:09:43 -0300

    feat: 86% done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count_apples: u32) -> u32 {
+    let mut apple_cust = 2;
+
+    if (count_apples > 40) {
+        apple_cust = 1;
+    }
+
+    count_apples * apple_cust
+}
 
 // Don't modify this function!
 #[test]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,21 +30,35 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(t) => output.push(append_bar(string, *t)),
+            }
             // TODO: Complete the function body. You can do it!
         }
         output
+    }
+
+    fn append_bar(s: &String, times: usize) -> String {
+        let mut result = s.clone();
+        for _ in 0..times {
+            result.push_str("bar");
+        }
+
+        result
     }
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
